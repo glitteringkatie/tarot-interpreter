7127ec8cfe7de7312009a00ebfc7ca0011b06bb0
Katie Hughes <katelhughes93@gmail.com>
2020-09-03 11:39:45 -0700

    DRY up arcana choice

--- src/App.test.js
+++ src/App.test.js
@@ -4,18 +4,14 @@ import App from "./App";
 
 test("fills in the cards for Major Arcana", () => {
   const { getByDisplayValue } = render(<App />);
-  const majorMinor = getByDisplayValue("-- major or minor? --");
-  expect(majorMinor).toBeInTheDocument();
-  fireEvent.change(majorMinor, { target: { value: "Major Arcana" } });
+  chooseArcana(getByDisplayValue, "Major Arcana");
   const card = getByDisplayValue("-- what value? --");
   expect(card).toMatchSnapshot();
 });
 
 test("fills in the cards for suits", () => {
   const { getByDisplayValue } = render(<App />);
-  const majorMinor = getByDisplayValue("-- major or minor? --");
-  expect(majorMinor).toBeInTheDocument();
-  fireEvent.change(majorMinor, { target: { value: "Swords" } });
+  chooseArcana(getByDisplayValue, "Swords");
   const card = getByDisplayValue("-- what value? --");
   expect(card).toMatchSnapshot();
 });
@@ -23,8 +19,15 @@ test("fills in the cards for suits", () => {
 // test("uses the given card predictions", () => {
 //   // mock major-arcana.js, check the magician gets called
 //   const { getByDisplayValue } = render(<App />);
-//   const majorMinor = getByDisplayValue("-- major or minor? --");
-//   fireEvent.change(majorMinor, { target: { value: "Major Arcana" } });
+//   chooseArcana(getByDisplayValue, "Major Arcana");
 //   const card = getByDisplayValue("-- what value? --");
 //   fireEvent.change(card, { target: { value: "The Magician" } });
 // });
+
+function chooseArcana(getByDisplayValue, option) {
+  const majorMinor = getByDisplayValue("-- major or minor? --");
+  expect(majorMinor).toBeInTheDocument();
+  fireEvent.change(majorMinor, { target: { value: option } });
+
+  return majorMinor;
+}
